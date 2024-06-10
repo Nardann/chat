54fe963ce63874822bfe99648b574387ef5e9550
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-10 18:53:47 +0200

    Update basedonnée.php

--- basedonnée.php
+++ basedonnée.php
@@ -1,17 +1,27 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-$servername = "127.0.0.1";
-$username = "u477148587_hugo"; // Remplacez par votre nom d'utilisateur MySQL
-$password = "C-est-le-0"; // Remplacez par votre mot de passe MySQL
-$dbname = "u477148587_hugo";
-
-// Créer la connexion
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Vérifier la connexion
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+  // Déclaration d'une nouvelle classe
+  class connexionDB {
+    private $host    = '127.0.0.1';  // nom de l'host  
+    private $name    = 'u477148587_hugo';    // nom de la base de donnée
+    private $user    = 'u477148587_hugo';       // utilisateur 
+    private $pass    = 'C-est-le-0';       // mot de passe (il faudra peut-être mettre '' sous Windows)
+    private $connexion;
+    
+    function __construct($host = null, $name = null, $user = null, $pass = null){
+      if($host != null){
+        $this->host = $host;           
+        $this->name = $name;           
+        $this->user = $user;          
+        $this->pass = $pass;
+      }
+      try{
+        $this->connexion = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->name,
+          $this->user, $this->pass, array(PDO::MYSQL_ATTR_INIT_COMMAND =>'SET NAMES UTF8', 
+          PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+      }catch (PDOException $e){
+        echo 'Erreur : Impossible de se connecter  à la BDD !';
+        die();
+      }
+    }
+  }
 ?>
