94ae08d0d689d9c9adb37fac362809c4cc0479b1
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-10 21:47:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ include('includes/header.php');
 <?php if (isset($_SESSION['username'])): ?>
     <h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
     <a href="login/logout.php">Logout</a>
+    <br>
+    <a href="friends/friends.php">View Friends</a>
+    <br>
+    <a href="friends/search.php">Search Users</a>
 <?php else: ?>
     <h1>Welcome to our website</h1>
     <a href="register/register.php">Register</a> | 
