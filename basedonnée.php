89ba49c58a9a4b72ad402d4d071a9aba991a35c5
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-10 18:57:07 +0200

    Update basedonnée.php

--- basedonnée.php
+++ basedonnée.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
   // Déclaration d'une nouvelle classe
   class connexionDB {
     private $host    = '127.0.0.1';  // nom de l'host  
