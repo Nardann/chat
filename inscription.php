30bd58d928513c3759a5286abc278ddd455c7eb9
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-10 18:14:36 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -1,5 +1,19 @@
 <?php
-include 'basedonnée.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$servername = "127.0.0.1";
+$username = "u477148587_hugo"; // Remplacez par votre nom d'utilisateur MySQL
+$password = "C-est-le-0"; // Remplacez par votre mot de passe MySQL
+$dbname = "u477148587_hugo";
+
+// Créer la connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nom = $_POST["nom"];
