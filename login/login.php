0e1f11a0de5874b85d3be9b71a5deaba9c1ed0c4
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-10 19:05:22 +0200

    Create login.php

---
+++ login/login.php
@@ -0,0 +1,11 @@
+<?php include('../includes/header.php'); ?>
+<form action="login_process.php" method="POST">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required>
+    <br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required>
+    <br>
+    <button type="submit">Login</button>
+</form>
+<?php include('../includes/footer.php'); ?>
