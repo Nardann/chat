7f9fea8599eef70cf8017e2c3578080007339349
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-10 18:11:36 +0200

    Update inscription.php

---
+++ inscription.php
@@ -0,0 +1,19 @@
+<?php
+include 'basedonnée.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nom = $_POST["nom"];
+    $prenom = $_POST["prenom"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    if ($conn->query($sql) === TRUE) {
+        echo "<script type='text/javascript'>alert('Inscriptionn effectuée');</script>";
+    } else {
+        echo "Erreur: " . $sql . "<br>" . $conn->error;
+    }
+
+
+    $conn->close();
+}
+?>
