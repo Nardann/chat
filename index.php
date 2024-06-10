975fb1346cf85715700a2e4ac6b6a92ac61a5e92
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-10 18:56:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-
+<?php
+  ﻿// Permet de savoir s'il y a une session. 
+  ﻿// C'est-à-dire si un utilisateur s'est connecté à votre site 
+  session_start(); 
+  
+  // Fichier PHP contenant la connexion à votre BDD
+  ﻿include('basedonnée.php'); 
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
