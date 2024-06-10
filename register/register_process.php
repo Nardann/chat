910eef4841d35a9b5b706d13533d0d43af07138a
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-10 19:11:04 +0200

    Update register_process.php

--- register/register_process.php
+++ register/register_process.php
@@ -3,12 +3,14 @@ include('../config/config.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
+    $name = $_POST['name'];
+    $firstname = $_POST['firstname'];
     $email = $_POST['email'];
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
-    $sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+    $sql = "INSERT INTO users (username, name, firstname, email, password) VALUES (?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sss", $username, $email, $password);
+    $stmt->bind_param("sss", $username, $name, $firstname, $email, $password);
 
     if ($stmt->execute()) {
         echo "Registration successful";
