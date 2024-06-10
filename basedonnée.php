80e1f7cd2f126572324a98abdb4984145dbb60e6
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-10 18:02:13 +0200

    Update basedonnée.php

--- basedonnée.php
+++ basedonnée.php
@@ -3,9 +3,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $servername = "127.0.0.1";
-$username = "u477148587_admin"; // Remplacez par votre nom d'utilisateur MySQL
-$password = "admin_DB_29"; // Remplacez par votre mot de passe MySQL
-$dbname = "u477148587_DB";
+$username = "u477148587_hugo"; // Remplacez par votre nom d'utilisateur MySQL
+$password = "C-est-le-0"; // Remplacez par votre mot de passe MySQL
+$dbname = "u477148587_hugo";
 
 // Créer la connexion
 $conn = new mysqli($servername, $username, $password, $dbname);
