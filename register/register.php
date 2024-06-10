7ef579bba50662b63c5fd8a822ba540ee0e35281
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-10 19:09:55 +0200

    Update register.php

--- register/register.php
+++ register/register.php
@@ -1,12 +1,16 @@
 <?php include('../includes/header.php'); ?>
 <form action="register_process.php" method="POST">
-    <label for="username">Username:</label>
+    <label for="username">Nom d'utilisateur:</label>
     <input type="text" id="username" name="username" required>
+    <label for="name">Nom:</label>
+    <input type="text" id="name" name="name" required>
+    <label for="firstname">Prénom:</label>
+    <input type="text" id="firstname" name="firstname" required>
     <br>
     <label for="email">Email:</label>
     <input type="email" id="email" name="email" required>
     <br>
-    <label for="password">Password:</label>
+    <label for="password">Mot de passe:</label>
     <input type="password" id="password" name="password" required>
     <br>
     <button type="submit">Register</button>
