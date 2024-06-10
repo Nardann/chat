bf27343c0c80d10ab5338014724549f3e251b277
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-10 19:01:22 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "my_database";
+$servername = "127.0.0.1";
+$username = "u477148587_hugo";
+$password = "C-est-le-0";
+$dbname = "u477148587_hugo";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+    
 ?>
