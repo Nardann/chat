241d0da248925e12d1b2eaf33d6f4415b6bfa5a7
Nardann <marclorente29@gmail.com>
2024-06-10 22:08:45 +0200

    Update 404.php

--- includes/404.php
+++ includes/404.php
@@ -3,16 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page Non trouvée</title>
+    <title>Page Not Found</title>
 </head>
 <body>
-    <h1>Error 404 - Page Non trouvée</h1>
-    <p>La page que vous recherchez n'existe pas.</p>
-    <p>Vous serez redirigé vers la page d'accueil dans 10 secondes.</p>
+    <h1>Error 404 - Page Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+    <p>You will be redirected to the homepage in <span id="countdown">10</span> seconds.</p>
     <script>
-        setTimeout(function() {
-            window.location.href = "../index.php";
-        }, 10000); // Redirection après 10 secondes (10000 millisecondes)
+        // Fonction pour mettre à jour le compte à rebours
+        function updateCountdown() {
+            var countdownElement = document.getElementById('countdown');
+            var countdownValue = parseInt(countdownElement.innerText);
+            countdownValue--;
+
+            // Si le compte à rebours atteint 0, rediriger vers la page d'accueil
+            if (countdownValue <= 0) {
+                window.location.href = "../index.php";
+            } else {
+                countdownElement.innerText = countdownValue;
+                setTimeout(updateCountdown, 1000); // Actualiser toutes les secondes
+            }
+        }
+
+        // Lancer la mise à jour initiale du compte à rebours
+        updateCountdown();
     </script>
 </body>
 </html>
