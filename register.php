30cbd363806acfed5bd8dc6310c3c31045e1c484
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-10 19:01:44 +0200

    Update and rename inscription.php to register.php

---
+++ register.php
@@ -0,0 +1,14 @@
+<?php include('../includes/header.php'); ?>
+<form action="register_process.php" method="POST">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required>
+    <br>
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required>
+    <br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required>
+    <br>
+    <button type="submit">Register</button>
+</form>
+<?php include('../includes/footer.php'); ?>
