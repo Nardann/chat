8cfb93d88b8ad75d3763690700f9729a49b05bab
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-11 16:37:58 +0200

    Update conversation.php

--- friends/conversation.php
+++ friends/conversation.php
@@ -68,7 +68,7 @@ $(document).ready(function() {
 document.addEventListener("DOMContentLoaded", function() {
 setTimeout(function() {
                 window.scrollTo(0, document.body.scrollHeight);
-            }, 1000);        });
+            }, 5000);        });
 </script>
 
 <?php include('../includes/footer.php'); ?>
