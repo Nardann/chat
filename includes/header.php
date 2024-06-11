6ffa056ddcff4c2668c303d42f95ccb8fb8a89e4
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-11 14:07:14 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -6,5 +6,5 @@
     <link rel="stylesheet" href="../css/styles.css">
     <title>My Website</title>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    </head>
+</head>
 <body>
