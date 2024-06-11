7a40a1da10957e5583b5765c89cae8c524a027d1
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-11 22:01:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ include('includes/navbar.php');
     <div class="container_index">
         <h1>Bienvenue !</h1>
         <div class="boutons_index">
-            <a class="bouton_index" href="register/register.php">Register</a> | 
+            <a class="bouton_index inscription" href="register/register.php">Register</a> | 
             <a class="bouton_index" href="login/login.php">Login</a>
         </div>
     </div>
