fb70196745b5c1dff18ac611427b4757598711a7
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-11 13:52:17 +0200

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,24 @@
-<nav id="nav-bar">
-   <a href="../../../index.php">Acceuil</a></li>
-   <a href="../../../friends/friends.php">Messages</a
-   <a href="../../../friends/search.php">Ajouter un ami</a>
-   <a href="../../../friends/friend_requests.php">Demande d'amis</a>
-    <button class="btn-deconnexion">Déconnexion</button>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
