1be364c73c8e0f0d2e2d3dd3db33dc6ca4078cf7
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-11 14:03:48 +0200

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,24 +1,8 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </div>
+<nav>
+    <ul>
+        <li><a href="../../../index.php">Acceuil</a></li>
+        <li><a href="../../../friends/friends.php">Messages</a></li>
+        <li><a href="../../../friends/search.php">Ajouter un ami</a></li>
+        <li><a href="../../../friends/friend_requests.php">Demande d'amis</a></li>
+    </ul>
 </nav>
