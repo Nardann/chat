369e77986be39339790391082f6bcea867a75315
Nardann <94087736+Nardann@users.noreply.github.com>
2024-06-11 16:08:08 +0200

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar">
     <div>
-    <a class="link_navbar" href="../../../index.php">Acceuil</a>
+    <a class="link_navbar" href="../../../index.php">Accueil</a>
     <a class="link_navbar" href="../../../friends/friends.php">Messages</a>
     <a class="link_navbar" href="../../../friends/search.php">Ajouter un ami</a>
     <a class="link_navbar" href="../../../friends/friend_requests.php">Demande d'amis</a>
