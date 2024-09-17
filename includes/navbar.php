f9ee3950bed9b3534b403ff121187db91196d2f1
Nardann <94087736+Nardann@users.noreply.github.com>
2024-09-17 21:25:19 +0200

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -3,9 +3,9 @@
         <!-- Navbar verticale centrée verticalement et horizontalement -->
         <nav class="col-2 navbar navbar-expand-lg navbar-light bg-light d-flex flex-column justify-content-center align-items-center vh-100">
             <div class="navbar-nav">
-                <a class="nav-link text-center" href="../../../index.php">Accueil</a>
-                <a class="nav-link text-center" href="../../../friends/friends.php">Messages</a>
-                <a class="nav-link text-center" href="../../../friends/search.php">Ajouter un ami</a>
+                <a class="nav-link text-center" href="../../../index.php">Accueil</a><br>
+                <a class="nav-link text-center" href="../../../friends/friends.php">Messages</a><br>
+                <a class="nav-link text-center" href="../../../friends/search.php">Ajouter un ami</a><br>
                 <a class="nav-link text-center" href="../../../friends/friend_requests.php">Demande d'amis</a>
             </div>
         </nav>
