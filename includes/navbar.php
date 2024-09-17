5f92e5d2bf55fd9d48dd36eda733e1ecb694d9f0
Nardann <94087736+Nardann@users.noreply.github.com>
2024-09-17 23:02:52 +0200

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,13 +1,22 @@
-<div class="container-fluid">
-    <div class="row">
-        <!-- Navbar verticale centrée verticalement et horizontalement -->
-        <nav class="col-2 navbar navbar-expand-lg navbar-light bg-light d-flex flex-column justify-content-center align-items-center vh-100">
-            <div class="navbar-nav">
-                <a class="nav-link text-center" href="../../../index.php">Accueil</a><br>
-                <a class="nav-link text-center" href="../../../friends/friends.php">Messages</a><br>
-                <a class="nav-link text-center" href="../../../friends/search.php">Ajouter un ami</a><br>
-                <a class="nav-link text-center" href="../../../friends/friend_requests.php">Demande d'amis</a>
-            </div>
-        </nav>
-        <div class="col-10">
-            <div class="content">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse flex-column" id="sidebar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="../../../index.php">Accueil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../../../friends/friends.php">Messages</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../../../friends/search.php">Ajouter un ami</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../../../friends/friend_requests.php">Demande d'amis</a>
+      </li>
+    </ul>
+  </div>
+</nav>
