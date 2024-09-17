4830845eedfe6fd7b13c705fb7efe071c63bf424
Nardann <94087736+Nardann@users.noreply.github.com>
2024-09-17 21:22:30 +0200

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,11 +1,11 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light flex-column vh-100">
-  <div class="container-fluid">
-    <div class="navbar-nav flex-column">
-      <a class="nav-link" href="../../../index.php">Accueil</a>
-      <a class="nav-link" href="../../../friends/friends.php">Messages</a>
-      <a class="nav-link" href="../../../friends/search.php">Ajouter un ami</a>
-      <a class="nav-link" href="../../../friends/friend_requests.php">Demande d'amis</a>
-    </div>
-  </div>
-</nav>
-<div class="space"></div>
+<div class="container-fluid">
+    <div class="row">
+        <!-- Navbar verticale à gauche -->
+        <nav class="col-2 navbar navbar-expand-lg navbar-light bg-light flex-column">
+            <div class="navbar-nav">
+                <a class="nav-link" href="../../../index.php">Accueil</a>
+                <a class="nav-link" href="../../../friends/friends.php">Messages</a>
+                <a class="nav-link" href="../../../friends/search.php">Ajouter un ami</a>
+                <a class="nav-link" href="../../../friends/friend_requests.php">Demande d'amis</a>
+            </div>
+        </nav>
