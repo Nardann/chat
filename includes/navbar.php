7f8806753c69e22eb08bc3129c67f70c8bd2a96d
Nardann <94087736+Nardann@users.noreply.github.com>
2024-09-17 23:05:24 +0200

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -36,18 +36,7 @@
         </a>
       </li>
     </ul>
-    <hr>
-    <div class="dropdown">
-      <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" class="rounded-circle me-2" width="32" height="32">
-        <strong>mdo</strong>
-      </a>
-      <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
-        <li><a class="dropdown-item" href="#">New project...</a></li>
-        <li><a class="dropdown-item" href="#">Settings</a></li>
-        <li><a class="dropdown-item" href="#">Profile</a></li>
-        <li><hr class="dropdown-divider"></li>
-        <li><a class="dropdown-item" href="#">Sign out</a></li>
-      </ul>
+
+
     </div>
 
