d59f3bda6ce544c912200252c72c7d1eced2eabd
Nardann <94087736+Nardann@users.noreply.github.com>
2024-09-17 19:33:49 +0200

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,9 +1,22 @@
-<nav class="navbar">
-    <div>
-    <a class="link_navbar" href="../../../index.php">Accueil</a>
-    <a class="link_navbar" href="../../../friends/friends.php">Messages</a>
-    <a class="link_navbar" href="../../../friends/search.php">Ajouter un ami</a>
-    <a class="link_navbar" href="../../../friends/friend_requests.php">Demande d'amis</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <!-- Contenu de la navbar aligné à gauche -->
+        <div class="navbar-collapse">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="../../../index.php">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../../../friends/friends.php">Messages</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../../../friends/search.php">Ajouter un ami</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../../../friends/friend_requests.php">Demande d'amis</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
 <div class="space"></div>
