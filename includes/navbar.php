d784aae4ece230fd8eb4642f1f6e1d36b80c0cc1
Nardann <94087736+Nardann@users.noreply.github.com>
2024-09-17 21:24:31 +0200

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,12 +1,12 @@
 <div class="container-fluid">
     <div class="row">
-        <!-- Navbar verticale à gauche -->
-        <nav class="col-2 navbar navbar-expand-lg navbar-light bg-light flex-column">
+        <!-- Navbar verticale centrée verticalement et horizontalement -->
+        <nav class="col-2 navbar navbar-expand-lg navbar-light bg-light d-flex flex-column justify-content-center align-items-center vh-100">
             <div class="navbar-nav">
-                <a class="nav-link" href="../../../index.php">Accueil</a>
-                <a class="nav-link" href="../../../friends/friends.php">Messages</a>
-                <a class="nav-link" href="../../../friends/search.php">Ajouter un ami</a>
-                <a class="nav-link" href="../../../friends/friend_requests.php">Demande d'amis</a>
+                <a class="nav-link text-center" href="../../../index.php">Accueil</a>
+                <a class="nav-link text-center" href="../../../friends/friends.php">Messages</a>
+                <a class="nav-link text-center" href="../../../friends/search.php">Ajouter un ami</a>
+                <a class="nav-link text-center" href="../../../friends/friend_requests.php">Demande d'amis</a>
             </div>
         </nav>
         <div class="col-10">
