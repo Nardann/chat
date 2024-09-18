7c6a10b96a1473f642739563a8b7acedc6b0c758
Nardann <94087736+Nardann@users.noreply.github.com>
2024-09-18 18:32:41 +0200

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -27,7 +27,8 @@
                 <hr>
                 <div class="dropdown pb-4">
                     <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
+                        <!--<img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">-->
+                        <i class="bi bi-person-bounding-box"></i>
                         <span class="d-none d-sm-inline mx-1"><?php echo $_SESSION['username']; ?></span>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
