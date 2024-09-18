f5e6c42646a1aee985c77dbb62b10093321ee413
Nardann <94087736+Nardann@users.noreply.github.com>
2024-09-18 21:07:57 +0200

    Update register.php

--- register/register.php
+++ register/register.php
@@ -5,22 +5,31 @@ redirectIfLoggedIn();
 include('../config/config.php');
 include('../includes/header.php'); 
 ?>
-<form action="register_process.php" method="POST">
-    <label for="username">Nom d'utilisateur:</label>
-    <input type="text" id="username" name="username" required>
-    <br>
-    <label for="name">Nom:</label>
-    <input type="text" id="name" name="name" required>
-    <br>
-    <label for="firstname">Prénom:</label>
-    <input type="text" id="firstname" name="firstname" required>
-    <br>
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required>
-    <br>
-    <label for="password">Mot de passe:</label>
-    <input type="password" id="password" name="password" required>
-    <br>
-    <button type="submit">Register</button>
-</form>
+<div class="d-flex justify-content-center align-items-center vh-100">
+    <div class="col-12 col-md-6 bg-light p-4 rounded shadow">
+        <form action="register_process.php" method="POST">
+            <div class="form-group mb-3">
+                <label for="username">Nom d'utilisateur:</label>
+                <input type="text" class="form-control" id="username" name="username" required>
+            </div>
+            <div class="form-group mb-3">
+                <label for="name">Nom:</label>
+                <input type="text" class="form-control" id="name" name="name" required>
+            </div>
+            <div class="form-group mb-3">
+                <label for="firstname">Prénom:</label>
+                <input type="text" class="form-control" id="firstname" name="firstname" required>
+            </div>
+            <div class="form-group mb-3">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
+            <div class="form-group mb-3">
+                <label for="password">Mot de passe:</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+            <button type="submit" class="btn btn-primary w-100">S'inscrire</button>
+        </form>
+    </div>
+</div>
 <?php include('../includes/footer.php'); ?>
