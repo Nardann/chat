106ab9666b33652bb164d1a2e00db8885bc0767e
Nardann <94087736+Nardann@users.noreply.github.com>
2024-09-18 21:52:15 +0200

    Create test.php

---
+++ friends/pictures/test.php
@@ -0,0 +1,5 @@
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <label for="fileUpload">Choisir une image à uploader :</label>
+    <input type="file" name="fileUpload" id="fileUpload">
+    <input type="submit" value="Uploader l'image">
+</form>
