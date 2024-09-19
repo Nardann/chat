07ac9732be9e4decec96ac56cad3030fb18f40ac
Nardann <94087736+Nardann@users.noreply.github.com>
2024-09-19 18:10:29 +0200

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,4 @@
-<div class="container-fluid">
+<div class="container-fluid nav_bar">
     <div class="row flex-nowrap">
         <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
             <div class="d-flex flex-column justify-content-between text-white min-vh-100">
