a6bbc97a38e5809ab73709cc81f010a027de9040
Nardann <94087736+Nardann@users.noreply.github.com>
2024-09-20 13:27:16 +0200

    Update conversation.php

--- friends/conversation.php
+++ friends/conversation.php
@@ -22,7 +22,7 @@ $friend_username = $result->fetch_assoc()['username'];
 $stmt->close();
 ?>
 
-<h2 style="position:fixed;">Conversation with <?php echo $friend_username; ?></h2>
+<h2 style="position:fixed; bottom:20px;">Conversation with <?php echo $friend_username; ?></h2>
 <div id="messages"></div>
 
 <form class="messageForm" id="messageForm">
